6493e32b8b13e2c8c797e5980e91a09f82b17497
josephinespit <josephinespit98@gmail.com>
2026-03-27 13:24:48 +0100

    fix base path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig({
   plugins: [svelte()],
-  // Replace 'repo-name' with your actual GitHub repository name
-  base: '/initiatievenkaart/', 
+  base: './', 
 })
